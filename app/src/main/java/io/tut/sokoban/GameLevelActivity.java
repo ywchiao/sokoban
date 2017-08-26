d413c5dc7eb7d736e603cc922aa03fd06af0e08b
Yiwei Chiao <ywchiao@gmail.com>
2017-08-27 01:42:56 +0800

    加上 GridView

--- app/src/main/java/io/tut/sokoban/GameLevelActivity.java
+++ app/src/main/java/io/tut/sokoban/GameLevelActivity.java
@@ -2,12 +2,21 @@ package io.tut.sokoban;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class GameLevelActivity extends AppCompatActivity {
+
+    String [] levelList = new String[] {"第一關", "第二關", "第三關", "第四關"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_level);
+
+        GridView gv_levels = (GridView) findViewById(R.id.gv_game_levels);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, levelList);
+        gv_levels.setAdapter(arrayAdapter);
     }
 }
