c4a7943a0046dfb986f1c1ed7d88e049379b06dd
Yiwei Chiao <ywchiao@gmail.com>
2017-08-26 11:58:39 +0800

    '遊戲介紹' 按鈕加了 Toast 訊息

--- app/src/main/java/io/tut/sokoban/MainActivity.java
+++ app/src/main/java/io/tut/sokoban/MainActivity.java
@@ -2,6 +2,9 @@ package io.tut.sokoban;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGameIntro = (Button) findViewById(R.id.btn_game_intro);
+
+        btnGameIntro.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(
+                    MainActivity.this,
+                    "按了遊戲簡介按鈕",
+                    Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
