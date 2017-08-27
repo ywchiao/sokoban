05264e7ce3a818d791f111e4c77e8f77728856d7
Yiwei Chiao <ywchiao@gmail.com>
2017-08-27 15:36:48 +0800

    緮製網格線

--- app/src/main/java/io/tut/sokoban/GameView.java
+++ app/src/main/java/io/tut/sokoban/GameView.java
@@ -2,6 +2,7 @@ package io.tut.sokoban;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -13,6 +14,12 @@ class GameView extends View {
         super(context);
     }
 
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+
+        mCellWidth = w / CELL_NUM_PER_LINE;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -22,5 +29,18 @@ class GameView extends View {
         // 背景色
         background.setColor(getResources().getColor(R.color.background));
         canvas.drawRect(0, 0, getWidth(), getHeight(), background);
+
+        Paint linePaint = new Paint();
+
+        // 繪制遊戲區域
+        linePaint.setColor(Color.BLACK);
+
+        for (int r = 0; r <= CELL_NUM_PER_LINE; r++) {
+            canvas.drawLine(0, r * mCellWidth, getWidth(), r * mCellWidth, linePaint);
+        }
+
+        for (int c = 0; c <= CELL_NUM_PER_LINE; c++) {
+            canvas.drawLine(c * mCellWidth, 0, c * mCellWidth, CELL_NUM_PER_LINE * mCellWidth, linePaint);
+        }
     }
 }
