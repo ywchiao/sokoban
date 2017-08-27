bfa47b04d62ea5261a2089e7ebf65441f57f7e48
Yiwei Chiao <ywchiao@gmail.com>
2017-08-27 18:43:36 +0800

    搬運工向下移動

--- app/src/main/java/io/tut/sokoban/GameView.java
+++ app/src/main/java/io/tut/sokoban/GameView.java
@@ -8,12 +8,16 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 class GameView extends View {
     private float mCellWidth;
     public static final int CELL_NUM_PER_LINE = 12;
     private Bitmap tsBitmap = null;
+
+    private int mManRow = 0;
+    private int mManColumn = 0;
 
     // Tile Size
     private static final int TILE_WIDTH = 64;
@@ -22,13 +26,6 @@ class GameView extends View {
         super(context);
 
         tsBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sokoban);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
-        super.onSizeChanged(w, h, oldW, oldH);
-
-        mCellWidth = w / CELL_NUM_PER_LINE;
     }
 
     @Override
@@ -57,8 +54,50 @@ class GameView extends View {
         // 複製搬運工
         Rect srcRect = new Rect(0, TILE_WIDTH * 2, TILE_WIDTH, TILE_WIDTH * 3);
 
-        Rect destRect = new Rect(0, 0, (int)mCellWidth, (int)mCellWidth);
+        Rect destRect = getRect(mManRow, mManColumn);
 
         canvas.drawBitmap(tsBitmap, srcRect, destRect, null);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+
+        mCellWidth = w / CELL_NUM_PER_LINE;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() != MotionEvent.ACTION_DOWN) {
+            return true;
+        }
+
+        int touch_x = (int)event.getX(); // 觸摸點的 x 坐標
+        int touch_y = (int)event.getY(); // 觸摸點的 y 坐標
+
+        if (touch_below_to_man(touch_x, touch_y, mManRow, mManColumn)) {
+            mManRow ++;
+        }
+
+        postInvalidate();
+
+        return true;
+    }
+
+    private Rect getRect(int row, int column) {
+        int left = (int)(column * mCellWidth);
+        int top = (int)(row * mCellWidth);
+        int right = (int)((column + 1) * mCellWidth);
+        int bottom = (int)((row + 1) * mCellWidth);
+
+        return new Rect(left, top, right, bottom);
+    }
+
+    private boolean touch_below_to_man(int touch_x, int touch_y, int manRow, int manColumn) {
+        int belowRow = manRow + 1;
+
+        Rect belowRect = getRect(belowRow, manColumn);
+
+        return belowRect.contains(touch_x, touch_y);
     }
 }
