f51adcf1c03cf4015550226a275c4c81978d1e59
Yiwei Chiao <ywchiao@gmail.com>
2017-08-27 14:59:26 +0800

    加入 GameActivity

--- app/src/main/java/io/tut/sokoban/GameLevelActivity.java
+++ app/src/main/java/io/tut/sokoban/GameLevelActivity.java
@@ -1,7 +1,10 @@
 package io.tut.sokoban;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
@@ -14,13 +17,25 @@ public class GameLevelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_level);
 
-        GridView gv_levels = (GridView) findViewById(R.id.gv_game_levels);
+        GridView gv_levels = (GridView) findViewById(R.id.gv_levels);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
             this,
             R.layout.gv_levels_item_textview,
             levelList
         );
+
         gv_levels.setAdapter(arrayAdapter);
+
+        gv_levels.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(GameLevelActivity.this, GameActivity.class);
+
+                intent.putExtra(GameActivity.KEY_SELECTED_LEVEL, i + 1);
+
+                startActivity(intent);
+            }
+        });
     }
 }
