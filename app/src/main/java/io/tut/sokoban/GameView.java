f6df1d342b249170477420238e8fbfdd0f40f677
Yiwei Chiao <ywchiao@gmail.com>
2017-08-27 18:05:15 +0800

    緮製搬運工

--- app/src/main/java/io/tut/sokoban/GameView.java
+++ app/src/main/java/io/tut/sokoban/GameView.java
@@ -1,19 +1,30 @@
 package io.tut.sokoban;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.View;
 
 class GameView extends View {
     private float mCellWidth;
     public static final int CELL_NUM_PER_LINE = 12;
+    private Bitmap tsBitmap = null;
+
+    // Tile Size
+    private static final int TILE_WIDTH = 64;
 
     public GameView(Context context) {
         super(context);
+
+        tsBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sokoban);
     }
 
+    @Override
     protected void onSizeChanged(int w, int h, int oldW, int oldH) {
         super.onSizeChanged(w, h, oldW, oldH);
 
@@ -35,12 +46,19 @@ class GameView extends View {
         // 繪制遊戲區域
         linePaint.setColor(Color.BLACK);
 
-        for (int r = 0; r <= CELL_NUM_PER_LINE; r++) {
+        for (int r = 0; r <= CELL_NUM_PER_LINE; r ++) {
             canvas.drawLine(0, r * mCellWidth, getWidth(), r * mCellWidth, linePaint);
         }
 
-        for (int c = 0; c <= CELL_NUM_PER_LINE; c++) {
+        for (int c = 0; c <= CELL_NUM_PER_LINE; c ++) {
             canvas.drawLine(c * mCellWidth, 0, c * mCellWidth, CELL_NUM_PER_LINE * mCellWidth, linePaint);
         }
+
+        // 複製搬運工
+        Rect srcRect = new Rect(0, TILE_WIDTH * 2, TILE_WIDTH, TILE_WIDTH * 3);
+
+        Rect destRect = new Rect(0, 0, (int)mCellWidth, (int)mCellWidth);
+
+        canvas.drawBitmap(tsBitmap, srcRect, destRect, null);
     }
 }
